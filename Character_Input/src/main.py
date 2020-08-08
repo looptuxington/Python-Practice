6f12f0baf9f0eec4e8e609d35243816c5723fa1c
looptuxington <looptuxington@riseup.net>
2020-08-08 06:48:51 -0500

    Add first practice

---
+++ Character_Input/src/main.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+age = int(input(' What is your age?'))
+year = 2120
+age_to_100 = str(year - age)
+print('Hello ' + name + '. You will be 100 in ' + age_to_100 + '.')
